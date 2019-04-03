2d9fa77e65ca5a8e1535770d0a131b2a290cfd1f
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-04-03 18:04:08 +0530

    Added SelectBox

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 import requestCall from '../js/fetchData.js';
 
+//On change load
+const sourceSelectBox = document.getElementById("newsSource");
+
+sourceSelectBox.onchange = function() {
+    const newsCategory = this.value;
+    const url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
+    instance.apiFetcher(url);
+  };
+
 const apiKey = "720c0314e8b2423eb7e1ffca5a1eeeb1";
 const newsCategory = "bbc-news";
 const url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
