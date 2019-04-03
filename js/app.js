3546d089378e9bd455cede6854b4c5e344fb0816
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-04-03 17:40:19 +0530

    Commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-import requestCall from '/js/fetchData.js';
+import requestCall from '../js/fetchData.js';
 
+const apiKey = "720c0314e8b2423eb7e1ffca5a1eeeb1";
 const newsCategory = "bbc-news";
 const url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
 requestCall.getRequest(url);
--- js/fetchData.js
+++ js/fetchData.js
@@ -1,16 +1,47 @@
 "use strict";
 class RequestService {
+
     async getRequest(url){
-      let data = await (await (fetch(url)
-        .then(res => {
-          return res.json()
+      const apiData = await (await (fetch(url)
+        .then(data => {
+          return data.json()
         })
         .catch(err => {
           console.log('Error: ', err)
         })
       ))
-      return data
+      newsSource.articlesProvider(apiData)
     }
 }
+class newsSourceProvider extends RequestService {
+    constructor(data) {
+      super(data);
+    }
+
+    articlesProvider(data) {
+        const elementId = document.getElementById("newsDetails");
+        let returnHtml = "",
+          uniqueVal = [];
+        if (data.status === "error") {
+          returnHtml = `<div id="error">${data.message}</div>`;
+        } else {
+          data.articles.map(({author, title, description, publishedAt, url, urlToImage}, index) => {
+            uniqueVal = index === 0 ? `<h1>${author}</h1>` : "";
+            returnHtml += `${uniqueVal}<div class="newsTitle">${
+                title
+            }</div><div class="newsDescription">${
+              description
+            }</div><div class="publishDate">${
+              publishedAt
+            }</div><div class="imageContainer"><a href="${
+              url
+            }" target="_blank"><img src=${urlToImage} /></a></div>`;
+          });
+        }
+        elementId.innerHTML = returnHtml;
+      }
+
+}
 const requestCall = new RequestService();
+const newsSource = new newsSourceProvider();
 export default requestCall;
