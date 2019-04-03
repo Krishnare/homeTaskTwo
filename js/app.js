23a23e195aa067fc7a0a2586e9037b11a634206e
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-04-03 16:50:13 +0530

    app js Changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import requestCall from 'js/fetchData.js';
+import requestCall from '/js/fetchData.js';
 
 const newsCategory = "bbc-news";
 const url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
